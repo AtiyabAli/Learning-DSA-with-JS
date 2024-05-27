494f736cee2fe29bfa5263635d0585dcd9265cc2
AtiyabAli <syedatiyabali@gmail.com>
2024-05-27 19:07:45 +0530

    Section-2 completed (understood Big O Time and space cpmplexity)

---
+++ Big O Notation/index.js
@@ -0,0 +1,52 @@
+// function sum(n) {
+//   let total = 0;
+//   for (let i = 0; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+// console.log(sum(5)); 
+
+// function add(n){
+//     return (n*(n+1))/2;
+// }
+// console.log(add(5));
+
+// let t1 = performance.now();
+
+// add(1000000);
+
+// let t2 = performance.now();
+
+// console.log(t2-t1); 
+//which one is better code
+//faster
+//Less memory-intensive
+//More readable
+
+//Count up and Down
+
+// function countUpDown(n){
+//     console.log("Gng up");
+//     for(let i=0; i<=n; i++){
+//         console.log(i);
+//     }
+//     console.log("At the top now gng down!!");
+//     for(let i=n; i>=0; i--){
+//         console.log(i);
+//     }
+//     console.log("Back down Bye!");
+// }
+// countUpDown(10)
+
+// function double(arr){
+//     let newArr = [];
+//     for(let i=0; i<arr.length; i++){
+//         newArr.push(2*arr[i])
+//     }
+//     return newArr;
+// }
+// console.log(double([1, 2 ,3 ,4]));
+
+
+
---
+++ Readme.txt
@@ -0,0 +1,12 @@
+Section 2 : Intro to Big O Notation
+
+Introducing Big O 
+
+O(1) - Constant
+O(n) - Linear
+O(n2)- Quadratic 
+ 
+Time complexity and Space complexity
+
+Most primitives (boolens, numbers, undefined, null)
+
