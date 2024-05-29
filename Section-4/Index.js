ebc5f1c848c5897e8331a2c9877dcaf66d862a5e
AtiyabAli <syedatiyabali@gmail.com>
2024-05-29 16:51:18 +0530

    updated

---
+++ Section-4/Index.js
@@ -0,0 +1,24 @@
+// function strCount(str, char){
+//     let arr = str.split("")
+//     let count = 0;
+//     let elm = ""
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]===char)
+//             count += 1
+//         elm = char[i]
+//     }
+//     console.log(elm);
+//     return count
+// }
+// console.log(strCount("syyed", "y"));
+
+//write a function which takes a str and return count of each character
+
+
+
+
+
+
+
+
+
