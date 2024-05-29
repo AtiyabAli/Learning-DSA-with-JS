fd2cd4fa28aa28ecf9fb686dc54c0bb050b1de93
AtiyabAli <syedatiyabali@gmail.com>
2024-05-29 23:57:25 +0530

    section 4 completed

--- Section-4/Index.js
+++ Section-4/Index.js
@@ -14,6 +14,26 @@
 
 //write a function which takes a str and return count of each character
 
+ //writes a function which takes in a string and returns an object 
+
+ function charCount(str){
+    let result = {};
+    for(let i=0; i<= str.length; i++){
+        let char = str[i];
+        if(result[char] > 0){
+            result[char]++
+        } else{
+            result[char] = 1;
+        }
+    }  
+    return result;
+ }
+console.log(charCount("sSsyesd"));
+
+
+
+
+
 
 
 
