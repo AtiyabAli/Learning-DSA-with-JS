8506362b9b443c6de31d1a51e834da139eb74d2d
AtiyabAli <syedatiyabali@gmail.com>
2024-06-04 00:10:13 +0530

    section-5 started

--- Section-5/Index.js
+++ Section-5/Index.js
@@ -1,15 +1,51 @@
-function same(arr1, arr2){
-   if(arr1.length !== arr2.length){
-    return false
-   } 
-   for(let i=0; i<arr1.length; i++){
-    let correctIndex = arr2.indexOf(arr1[i]**2)
-    if(correctIndex === -1){
-        return false
+//Frequency counter
+
+// function same(arr1, arr2){
+//    if(arr1.length !== arr2.length){
+//     return false
+//    }
+//    for(let i=0; i<arr1.length; i++){
+//     let correctIndex = arr2.indexOf(arr1[i]**2)
+//     if(correctIndex === -1){
+//         return false
+//     }
+//     arr2.splice(correctIndex, 1)
+//    }
+//    return true
+// }
+
+// console.log(same([2, 10, 5], [4, 100, 26]));
+
+// function validAnagram(first, second) {
+//   if (first.length !== second.length) {
+//     return false;
+//   }
+//   const lookup = {};
+
+//   for (let i = 0; i < first.length; i++) {
+//     let letter = first[i];
+//     lookup[letter] ? lookup[letter]++ : (lookup[letter] = 1);
+//   }
+//   for (let i = 0; i < second.length; i++) {
+//     let letter = second[i];
+//     if (!lookup[letter]) {
+//       return false;
+//     } else {
+//       lookup[letter] -= 1;
+//     }
+//   }
+//   return true;
+// }  
+// console.log(validAnagram("syed", "deys"));
+
+function sumZero(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[i]+arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
     }
-    arr2.splice(correctIndex, 1)
-   }
-   return true
 }
 
-console.log(same([2, 10, 5], [4, 100, 26]));
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 5]));
